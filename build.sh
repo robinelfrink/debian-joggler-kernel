c07cc85391314cb5fa0575b16925e62857939354
Robin Elfrink <robin.elfrink@eu.equinix.com>
2019-11-25 13:11:56 +0100

    Moved kernel configs from https://github.com/robinelfrink/debian-joggler.

---
+++ build.sh
@@ -0,0 +1,10 @@
+sed 's/^deb /deb-src /' /etc/apt/sources.list >> /etc/apt/sources.list
+apt-get --yes update
+apt-get --yes install build-essential fakeroot curl linux-source-${KERNEL_VERSION}
+apt-get --yes build-dep linux
+tar --extract --xz --touch --file /usr/src/linux-source-${KERNEL_VERSION}.tar.xz
+cd linux-source-${KERNEL_VERSION}
+cp ../config-${KERNEL_VERSION} .config
+for PATCH in ../patches/*.patch; do patch -p1 < ${PATCH}; done
+make oldconfig
+make deb-pkg
